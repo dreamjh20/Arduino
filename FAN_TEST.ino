860d100e8855f1a7755c8f15f37aea23e928627c
dreamjh20 <68009161+dreamjh20@users.noreply.github.com>
2021-05-04 01:38:58 -0900

    FAN test

--- FAN_TEST.ino
+++ FAN_TEST.ino
@@ -1,14 +1,18 @@
 #include <Wire.h> 
 
-#define fan 7
+#define fan 3
 void setup() {
   // put your setup code here, to run once:
   pinMode(fan, OUTPUT);
+  digitalWrite(fan, HIGH);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(fan, HIGH);
+  delay(200);
+  digitalWrite(fan, LOW);
+  delay(10);
   Serial.print("-\n");
 }
