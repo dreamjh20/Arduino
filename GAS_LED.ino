6fb044b8704a82375094dbbc4330bd40e108632e
dreamjh20 <68009161+dreamjh20@users.noreply.github.com>
2021-06-16 23:06:56 +0900

    switch

--- GAS_LED.ino
+++ GAS_LED.ino
@@ -1,22 +1,48 @@
 #define GAS 0
 #define LED 8
+#define BTN 7
 void setup() {
   Serial.begin(9600);
   pinMode(LED, OUTPUT);
   pinMode(GAS, INPUT);
+  pinMode(BTN, INPUT);
+  
 }
-
+int OnOff = 0;
 void loop() {
-  int val = analogRead(gas);
-  if(val >= 520){
-    
+  int val = analogRead(GAS);
+  
+  if(BTN == HIGH){
+    OnOff = 1 - OnOff;
+    Serial.print(OnOff);
+    delay(400);
+  }
+  
+  if(OnOff == 1){
     digitalWrite(LED, HIGH);
   }
+  
+  else{
+    digitalWrite(LED, LOW);
+    if(val >= 160){
+      digitalWrite(LED, HIGH);
+    }
+  }
 
-  if(val < 520) {
+
+  if(OnOff == 1){
+    if(BTN == HIGH){
+      OnOff = 1 - OnOff;
+    }
+    else{
+      digitalWrite(LED, HIGH);
+    }
+  }
+  else{
     digitalWrite(LED, LOW);
   }
 
+  
   Serial.println(val);
   delay(1000);
 }
