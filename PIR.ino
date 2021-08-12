c0bd6e1ca406eb687983fcc5d05433ecd711009b
dreamjh20 <68009161+dreamjh20@users.noreply.github.com>
2021-08-12 19:45:10 +0900

    Add files via upload

---
+++ PIR.ino
@@ -0,0 +1,21 @@
+int PIR = 5 ;  
+int Led = 13 ;
+
+void setup() {
+  Serial.begin(9600);        
+  pinMode(PIR, INPUT); 
+  pinMode(Led, OUTPUT); 
+  digitalWrite(Led ,LOW);    
+}
+void loop( ) {
+  if (digitalRead(PIR) == HIGH) {        
+    Serial.println("SENSOR ON");     
+    digitalWrite(Led , HIGH );
+    delay(200);        
+  }  
+  else {                                  
+    Serial.println("SENSOR OFF"); 
+    digitalWrite(Led , LOW );                  
+  }
+  delay(100);                             
+}
