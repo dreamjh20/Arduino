33fa067cab8f416f3e0c3849b05922385a3d9f1a
dreamjh20 <68009161+dreamjh20@users.noreply.github.com>
2021-08-13 14:41:23 +0900

    Add Dinosaur Macro
    Dinosaur game macro with cds sensor

---
+++ Dinosaur.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+#define CDS A0
+Servo sv;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(CDS,INPUT);
+  sv.attach(9);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int cds;
+  cds = analogRead(CDS);
+  Serial.println(cds);
+  sv.write(7);
+  delay(400);                             
+  sv.write(15);
+  delay(700);
+}
