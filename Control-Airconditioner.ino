c420aa14b146281ee682de3e01dee154c83d453d
dreamjh20 <68009161+dreamjh20@users.noreply.github.com>
2021-08-22 20:29:44 +0900

    FEAT: Show Temperature & Humidity
    show temperature and humidity with dht11 sensor using NodeMCU(wifi)

--- Control-Airconditioner.ino
+++ Control-Airconditioner.ino
@@ -38,25 +38,50 @@
 
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
+#include <DHT.h>
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "YourAuthToken";
+char auth[] = "HAv8MAVIyzS71MEtL0inCHeGbAkZ_zUd";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
 char ssid[] = "Moon";
 char pass[] = "mmmmmmmm";
 
+#define DHTPIN 2
+#define DHTTYPE DHT11
+ 
+DHT dht(DHTPIN, DHTTYPE);
+BlynkTimer timer;
+
+void sendSensor()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+ 
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Blynk.virtualWrite(V5, t);
+  Blynk.virtualWrite(V6, h);
+}
+  
 void setup()
 {
   // Debug console
   Serial.begin(9600);
 
   Blynk.begin(auth, ssid, pass);
+  dht.begin();
+ 
+  timer.setInterval(1000L, sendSensor);
 }
 
 void loop()
 {
   Blynk.run();
+  timer.run();
 }
